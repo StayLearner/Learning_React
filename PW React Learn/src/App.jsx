0e1d09f3e910464d662757c7973498b1c49011cf
Rohan Ghosh <156415986+a-AvgLearner@users.noreply.github.com>
2024-10-24 14:26:25 +0530

    Add files via upload

---
+++ PW React Learn/src/App.jsx
@@ -0,0 +1,17 @@
+import Card from './components/card/Card'
+import Grid from './components/grid/Grid'
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+      {/* <Card player="X" /> */}
+  
+     <Grid numberOfCards= {9} />
+
+      </>
+  )
+}
+
+export default App
